e563ecfe5f448684d6d7356502f35c378c279025
Liam Clarke <liamcclarke12@gmail.com>
2024-10-09 14:45:36 +0100

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace BuyItOnce.Server.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /Home/Welcome
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
