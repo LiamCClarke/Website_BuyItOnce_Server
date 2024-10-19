d6133e39c0bd37829334e951abfb6f5e0f83d8e9
Liam Clarke <liamcclarke12@gmail.com>
2024-10-20 00:12:25 +0100

    Created API controller, user accoutn model, and route test in .http file

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using BuyItOnce.Server.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuyItOnce.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        public AccountController()
+        {
+        }
+
+        [HttpPost("CreateAccount")]
+        public IActionResult CreateAccount(UserAccount user)
+        {
+            return BadRequest();
+        }
+    }
+}
---
+++ Models/UserAccount.cs
@@ -0,0 +1,11 @@
+﻿namespace BuyItOnce.Server.Models
+{
+    public class UserAccount
+    {
+        public int Id { get; set; }
+        public required string Username { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public DateTime? CreatedAt { get; set; }
+    }
+}
